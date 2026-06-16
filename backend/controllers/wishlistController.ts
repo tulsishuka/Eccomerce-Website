599f6d6a727c99619bad4d5f8190b3b443f6aa4c
Tulasi <tulsishuklag@gmail.com>
2026-06-16 12:56:23 +0530

    backend/controllers/wishlistController.ts

---
+++ backend/controllers/wishlistController.ts
@@ -0,0 +1,59 @@
+import { Request, Response } from "express";
+import { Wishlist } from "../models/wishlistModel";
+
+export const addWishlist = async (
+  req: Request,
+  res: Response
+) => {
+  const { userId, productId } = req.body;
+
+  const exists = await Wishlist.findOne({
+    user: userId,
+    product: productId,
+  });
+
+  if (exists) {
+    return res.json({
+      success: false,
+      message: "Already in wishlist",
+    });
+  }
+
+  const wishlist = await Wishlist.create({
+    user: userId,
+    product: productId,
+  });
+
+  res.json({
+    success: true,
+    wishlist,
+  });
+};
+
+export const getWishlist = async (
+  req: Request,
+  res: Response
+) => {
+  const wishlist = await Wishlist.find({
+    user: req.params.userId,
+  }).populate("product");
+
+  res.json({
+    success: true,
+    data: wishlist,
+  });
+};
+
+export const removeWishlist = async (
+  req: Request,
+  res: Response
+) => {
+  await Wishlist.findByIdAndDelete(
+    req.params.id
+  );
+
+  res.json({
+    success: true,
+    message: "Removed",
+  });
+};

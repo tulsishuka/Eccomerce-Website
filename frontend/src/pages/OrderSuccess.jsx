5f820e5672a7aac871021d340ff4a6927ebd49a0
Tulasi <tulsishuklag@gmail.com>
2026-06-18 13:54:35 +0530

    frontend/src/pages/OrderSuccess.jsx

---
+++ frontend/src/pages/OrderSuccess.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const OrderSuccess = () => {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center">
+
+      <h1 className="text-4xl font-bold text-green-600">
+        Order Placed Successfully 🎉
+      </h1>
+
+      <p className="mt-3 text-gray-500">
+        Thank you for shopping with us.
+      </p>
+
+      <Link
+        to="/"
+        className="mt-6 bg-black text-white px-8 py-3 rounded-lg"
+      >
+       Go To Home
+      </Link>
+
+    </div>
+  );
+};
+
+export default OrderSuccess;
